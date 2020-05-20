e87d84e0cb2ecaf1dc440c3ae66bd094607b7426
alfredopc26 <calfredoperez26@gmail.com>
2020-05-20 11:28:40 -0500

    cambiosEmpresa0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MenuComponent } from './hospital/menu/menu.component';
+import { TriageComponent } from './hospital/triage/triage.component';
 
 
 const routes: Routes = [ 
   { path: '', component: DashboardComponent },
   { path: 'hospital/:idHospital', component: MenuComponent },
+  { path: 'triage/:idHospital', component: TriageComponent },
 ];
 
 @NgModule({
